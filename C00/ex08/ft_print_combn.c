18eb58cba966457e1df9dd3be4f8d5b51ec34434
Zied Harzi <zharzi@student.42angouleme.fr>
2022-06-20 20:21:12 +0200

    20jun22

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -6,42 +6,69 @@
 /*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 08:28:11 by zharzi            #+#    #+#             */
-/*   Updated: 2022/06/20 11:34:13 by zharzi           ###   ########.fr       */
+/*   Updated: 2022/06/20 20:18:38 by zharzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
+
+void	ft_write_combn(int comb, int n)
+{
+	char	c;
+
+	c = '0';
+	if (--n)
+		ft_write_combn(comb / 10, n);
+	c = '0' + comb % 10;
+	write(1, &c, 1);
+}
+
+int	ft_check_combn(int comb, int n)
+{
+	int	x;
+	int	a;
+
+	x = 1;
+	a = n;
+	while (a)
+	{
+		x *= 10;
+		a--;
+	}
+	while (++a < n)
+	{
+		if ((comb % x) / (x / 10) >= (comb % (x / 10) / (x / 100)))
+			return (0);
+		x /= 10;
+	}
+	return (1);
+}
 
 void	ft_print_combn(int n)
 {
-	char	tab[10];
-	char	c;
-	int		i;
-	int		x;
+	int		comb;
+	int		last;
+	int		a;
 
-	i = -1;
-	c = '0';
-	while (++i < n)
-		tab[i] = c++;
-	tab[n] = '\0';
-	// poursuivre en int, gerer les 0 avec un modulo overflow par rapport a n
-	i = 0;
-	while (tab[i])
+	comb = n + 1;
+	last = 0;
+	a = 10 - n;
+	while (--comb)
 	{
-		x = x + tab[i] - '0';
-		if (tab[i + 1])
-			x = x * 10;
-		i++;
+		last *= 10;
+		last = last + a++;
 	}
-	printf("atoi %d\n", x);
-}
-
-int	main(void)
-{
-	int	n;
-
-	n = 3;
-	ft_print_combn(n);
-	return (0);
+	comb = -1;
+	a = -1;
+	while (++comb < last)
+	{
+		if (ft_check_combn(comb, n))
+		{
+			if (++a)
+				write(1, ", ", 2);
+			ft_write_combn(comb, n);
+		}
+	}
+	write(1, ", ", 2);
+	ft_write_combn(comb, n);
 }
