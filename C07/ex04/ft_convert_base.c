6e535632450681780d3179f42f5b43d6be95f1cd
zharzi <zharzi@student.42angouleme.fr>
2022-06-30 14:42:56 +0200

    30jun22

--- C07/ex04/ft_convert_base.c
+++ C07/ex04/ft_convert_base.c
@@ -6,7 +6,7 @@
 /*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 20:02:10 by zharzi            #+#    #+#             */
-/*   Updated: 2022/06/29 00:47:51 by zharzi           ###   ########.fr       */
+/*   Updated: 2022/06/29 20:39:02 by zharzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,45 +36,46 @@ void	ft_len_conv(int nbr, char *base, int *len)
 	}
 }
 
-char	*ft_nbr_base(char *s, int nbr, char *base, int *len)
+void	ft_int_base(char *s, int nbr, char *base, int *len)
 {
 	if (nbr < 0)
 	{
 		s[0] = '-';
+		s[*len - 1] = base[-nbr % ft_strlen(base)];
 		if (nbr <= (-ft_strlen(base)))
-			ft_nbr_base(s, nbr / -ft_strlen(base), base, len - 1);
-		s[*len] = base[-nbr % ft_strlen(base)];
+		{
+			*len -= 1;
+			ft_int_base(s, nbr / -ft_strlen(base), base, len);
+		}
 	}
 	else
 	{
+		s[*len - 1] = base[nbr % ft_strlen(base)];
 		if (nbr >= ft_strlen(base))
-			ft_nbr_base(s, nbr / ft_strlen(base), base, len - 1);
-		s[*len] = base[nbr % ft_strlen(base)];
+		{
+			*len -= 1;
+			ft_int_base(s, nbr / ft_strlen(base), base, len);
+		}
 	}
-	return (s);
 }
 
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
 	char	*res;
-	int		*len;
+	int		len;
 	int		n;
-	int		z;
+	int		i;
 
-	z = 0;
-	len = &z;
+	i = -1;
+	len = 0;
 	if (!ft_check_base(base_from) || !ft_check_base(base_to))
 		return (NULL);
 	n = ft_atoi_base(nbr, base_from);
-	ft_len_conv(n, base_to, len);
-	res = (char *)malloc(*len + 1);
-	res = ft_nbr_base(res, n, base_to, len);
-	free(res);
+	ft_len_conv(n, base_to, &len);
+	res = (char *)malloc(len + 1);
+	while (++i < len)
+		res[i] = i + '0';
+	res[len] = '\0';
+	ft_int_base(res, n, base_to, &len);
 	return (res);
-}
-
-int	main(void)
-{
-	printf("%s\n", ft_convert_base("ABCD", "0123456789ABCDEF", "01234567"));
-	return (0);
 }
